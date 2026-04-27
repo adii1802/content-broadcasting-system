c9f8af545b9b0d9881141304a1e5205f4b09d63a
Aditya Gaur <adityagaur1802@gmail.com>
2026-04-27 15:46:58 +0530

    ssl internal command

--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool(
   process.env.DATABASE_URL
-    ? {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    }
+    ? { connectionString: process.env.DATABASE_URL }
     : {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+      password: process.env.DB_PASSWORD
     }
 );
 
